cb4b3bc77cd11dc0b2ac0d1ef05ce9e3dadd4427
翁天俊 <justinwuny@163.com>
2019-11-07 03:29:38 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App'
+import VueAMap from 'vue-amap';
+import { lazyAMapApiLoaderInstance } from 'vue-amap';
+
+Vue.use(VueAMap);
+VueAMap.initAMapApiLoader({
+  key: '7d0ae7939300e7bef13d20b194cfcac7',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  v: '1.4.4'
+});
+Vue.config.productionTip = false
+
+lazyAMapApiLoaderInstance.load().then(() => {
+  this.map = new AMap.Map('map', {
+    center: new AMap.LngLat(121.59996, 31.197646)
+  });
+});
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
---
+++ pages/index/index.vue
@@ -0,0 +1,74 @@
+<template>
+	<view class="content">
+		<view id="map" />
+		<view class="chat-body">
+			<input class="chat-input" type="text" />
+			<button class="chat-send">发送</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	import {
+		lazyAMapApiLoaderInstance
+	} from 'vue-amap';
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			lazyAMapApiLoaderInstance.load().then(() => {
+				this.map = new AMap.Map('map', {
+					center: new AMap.LngLat(121.59996, 31.197646)
+				});
+			});
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.chat-body {
+		width: 100vw;
+		height: calc(15vh - 44px);
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		background: #66ccff;
+	}
+
+	.chat-body * {
+		flex-grow: 1;
+	}
+
+	.chat-input {
+		padding: 0rpx 30rpx;
+		height: calc(5vh + 11px);
+		background: #88DDFF;
+		color: #FFFFFF;
+		border-radius: 25rpx;
+	}
+	
+	.chat-send{
+		background: #AADDFF;
+	}
+
+	.content {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	#map {
+		height: 85vh;
+		width: 100vw;
+		display: flex;
+	}
+</style>
